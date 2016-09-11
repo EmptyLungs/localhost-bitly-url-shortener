73c022944c56c9b7db2dce914631ac2847e70c1d
Dmitry Pavlenko <dmitryrint@gmail.com>
2016-09-11 15:38:30 +0700

    Update bd_conn.php

--- p/bd_conn.php
+++ p/bd_conn.php
@@ -11,8 +11,4 @@ $server_name = "localhost";
 $username = "root";
 $password = "";
 
-$conn = mysqli_connect($server_name, $username, $password);
-if (!$conn){
-    die("connection failed". mysqli_connect_error());
-}
 $bd = new PDO('mysql:host=localhost;dbname=urls', $username, $password);
